7ebf2a2be363f1236c07dc981dfbb274c858733f
poikilos <7557867+poikilos@users.noreply.github.com>
2023-05-06 10:16:08 -0400

    Switch to KivyMD.

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ import os
 
 # from kivy.graphics import Color, Rectangle
 # from kivy.graphics import Line
-from kivy.app import App
+from kivymd.app import MDApp
 # from kivy.core.window import Window
 # from kivy.animation import Animation
-from kivy.factory import Factory
-
+# from kivy.factory import Factory
+from kivymd.uix.button import MDFloatingActionButton
+from kivymd.uix.boxlayout import MDBoxLayout
 # from kivy.graphics import Ellipse
 # from kivy.uix.image import Image
 # from kivy.core.image import Image as CoreImage
@@ -43,39 +44,52 @@ from rotocanvas.kivypixels.pixelwidget import PixelWidget
 from rotocanvas.kivypixels.colorpopup import ColorPopup
 
 
-class RotoCanvasApp(App):
+class RotoCanvasApp(MDApp):
 
     def build(self):
         self.pixelWidget = None
 
-        self.mainWidget = BoxLayout(orientation='horizontal')
+        self.mainWidget = MDBoxLayout(
+            orientation='horizontal',
+            md_bg_color='black',
+        )
 
         self.pixelWidget = PixelWidget()
         self.mainWidget.add_widget(self.pixelWidget)
 
-        self.buttonsLayout = BoxLayout(orientation='vertical',
-                                       size_hint=(.1, 1.0))
+        self.buttonsLayout = MDBoxLayout(
+            orientation='vertical',
+            size_hint=(.1, 1.0),
+            spacing='8dp',
+            padding=('8dp','8dp','8dp','8dp'),
+        )
         self.mainWidget.add_widget(self.buttonsLayout)
 
         self.paletteWidget = ColorPopup(self.choseColor,
                                         size_hint=(.9, .8))
 
-        self.saveButton = Factory.Button(text="Save")
+        self.saveButton = MDFloatingActionButton(
+            text="Save",
+            icon='content-save',
+        )
         # id="saveButton"
         self.buttonsLayout.add_widget(self.saveButton)
         self.saveButton.bind(on_press=self.pixelWidget.onSaveButtonClick)
 
-        self.paletteButton = Factory.Button(text="Palette")
+        self.paletteButton = MDFloatingActionButton(
+            text="Palette",
+            icon='palette',
+        )
         # id="paletteButton"
         self.buttonsLayout.add_widget(self.paletteButton)
         self.paletteButton.bind(on_press=self.paletteWidget.open)
         '''
-        # self.colorButton = Factory.Button(text="Color",
+        # self.colorButton = MDFloatingActionButton(text="Color",
                                           # id="colorButton")
         # self.buttonsLayout.add_widget(self.colorButton)
         # self.colorButton.bind(on_press=self.pixelWidget.onColorButtonClick)
 
-        # self.eraserButton = Factory.Button(text="Eraser",
+        # self.eraserButton = MDFloatingActionButton(text="Eraser",
                                            # id="eraserButton")
         # self.buttonsLayout.add_widget(self.eraserButton)
         # self.eraserButton.bind(
