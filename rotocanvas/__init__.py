6f7e660001492698f80362bfc19bf283984da595
poikilos <7557867+poikilos@users.noreply.github.com>
2024-03-17 19:32:00 -0400

    Add no_enclosures and a related test.

--- rotocanvas/__init__.py
+++ rotocanvas/__init__.py
@@ -55,11 +55,35 @@ def echo2(*args, **kwargs):
         return
     print(*args, file=sys.stderr, **kwargs)
 
+
 MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
 ASSETS_DIR = os.path.join(MODULE_DIR, "assets")
 BRUSH_IMAGES_DIR = os.path.join(ASSETS_DIR, "brushes")
 if not os.path.isdir(BRUSH_IMAGES_DIR):
     raise FileNotFoundError(BRUSH_IMAGES_DIR)
+
+DEFAULT_ENCLOSURES = (
+    ("'", "'"),
+    ('"', '"'),
+    ('[', ']'),
+    ('(', ')'),
+    ('{', '}'),
+)
+
+
+def no_enclosures(value, pairs=None):
+    if not pairs:
+        pairs = DEFAULT_ENCLOSURES
+    if not value:
+        return value
+    if len(value) < 2:
+        return value
+    for pair in pairs:
+        if value.startswith(pair[0]) and value.endswith(pair[1]):
+            # Remove quotes & replace escaped ender with ender:
+            value = value[1:-1].replace('\\'+pair[1], pair[1])
+    return value
+
 
 def main(args):
     print("# You tried to run the module.")
---
+++ tests/rotocanvas/test_module.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import unittest
+# import sys
+
+from rotocanvas import (
+    no_enclosures,
+    DEFAULT_ENCLOSURES,
+)
+
+
+class ModuleTest(unittest.TestCase):
+    def test_no_enclosures(self):
+        pairs = DEFAULT_ENCLOSURES
+        good_strings = [
+            "A",
+            "abcd",
+            'ab"cd',
+            "ab)cd",
+        ]
+        # enclosed_strings = {}
+        for original in good_strings:
+            # enclosed_strings[original] = []
+            for pair in pairs:
+                enclosed = (
+                    pair[0]
+                    + original.replace(pair[1], '\\'+pair[1])
+                    + pair[1]
+                )
+                # enclosed_strings[original].append(enclosed)
+                self.assertEqual(no_enclosures(enclosed), original)
+
+
+if __name__ == "__main__":
+    print("Error: You must run this from the repo directory via:")
+    print("python3 -m nose")
